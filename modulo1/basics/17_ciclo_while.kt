12db4a044d7d061a6ecc366fcbbcb664fa30638f
natcore-tech <elihu.navarrete@ute.edu.ec>
2026-04-15 11:01:10 -0500

    clase-15-04-2026

--- modulo1/basics/17_ciclo_while.kt
+++ modulo1/basics/17_ciclo_while.kt
@@ -3,12 +3,27 @@
 fun main() {
   println("Controles de Flujo Iteraciones, Ciclos repetitivos ciclo while")
   
-  println("While basico")
-  var contador = 1
-  while(contador <= 5){
+ print("Do While")
+  contador = 1
+  do {
       println(contador)
       contador++;
+  }while(contador <= 5)
+  
+  println("Break Contiune")
+  contador = 1
+  while(contador <= 10){
+      contador ++;
+      if(contador==3) continue
+      if(contador==7) break
+      println(contador)
   }
   
-  
+  var input: String
+  while(true){
+      println("Escribe 'salir' para terminar: ")
+      input=readLine()?:""
+      if (input=="salir") break
+      println("usted ingreso: $input")
+  }
 }
