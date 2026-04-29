958da5d1b312b132dd1e4111ffa91c5dd6bc0d84
natcore-tech <elihu.navarrete@ute.edu.ec>
2026-04-29 11:09:31 -0500

    clase-29-04-2026

--- modulo2/tts/app/build.gradle.kts
+++ modulo2/tts/app/build.gradle.kts
@@ -18,6 +18,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        externalNativeBuild {
+            cmake {
+                cppFlags += ""
+            }
+        }
     }
 
     buildTypes {
@@ -32,6 +37,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
     }
 }
 
